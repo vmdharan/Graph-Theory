e3e1e26e8cd96b4af22ca3cf7d4a14d8c4c5818a
vmdharan <rexar7@gmail.com>
2016-12-17 18:15:43 +1100

    Query edges where vertex 'x' is one of the nodes.
    - Query edges where vertex 'x' is one of the nodes.
    - Add comments.

--- src/graph_exec.js
+++ src/graph_exec.js
@@ -6,7 +6,10 @@
 // Import the Graph object.
 var graph = require('./Graph');
 
+// Create a new Graph instance.
 var G = new graph.Graph();
+
+// Add vertices to the graph.
 G.addVertex('a');
 G.addVertex('b');
 G.addVertex('c');
@@ -14,10 +17,24 @@ G.addVertex('x');
 G.addVertex('y');
 G.addVertex('z');
 
+// Display vertices.
 console.log(G.V);
+console.log('\n');
 
+// Add edges to the graph.
 G.addEdge(0.1, 'x', 'y');
 G.addEdge(0.2, 'y', 'z');
 G.addEdge(0.3, 'a', 'x');
 G.addEdge(0.4, 'b', 'c');
+
+// Display edges.
 console.log(G.E);
+console.log('\n');
+
+// Display edges where vertex 'x' is either a source or destination.
+for(var i=0;i<G.E.length;i++) {
+	if((G.E[i]['source'] == 'x') || (G.E[i]['destination'] == 'x')) {
+		console.log(G.E[i]);
+	}
+}
+console.log('\n');
